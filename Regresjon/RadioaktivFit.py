b4384de2027b5c4525ae45766addb7fc4c3ed8b7
Sølve Selstø <124062842+Soelve@users.noreply.github.com>
2024-09-02 15:06:42 +0200

    Update RadioaktivFit.py

--- Regresjon/RadioaktivFit.py
+++ Regresjon/RadioaktivFit.py
@@ -1,10 +1,9 @@
-"""
- Dette skriptet tar utgangspunkt i eit gitt sett med data for 
- radioaktivitet og brukar dette til å estimere ein eksponentiell
- samanheng mellom tid og radioaktivitet. Denne kan, i sin tur, brukast
- til å estimere halveringstida for den radioaktive isotopen.
- Vi gjer dette ved å bruke lineær regresjon med minste kvadraters metode
- på logaritmen (den naturlege logaritmen) på dei sampla data. 
+"""Dette skriptet tar utgangspunkt i eit gitt sett med data for 
+radioaktivitet og brukar dette til å estimere ein eksponentiell
+samanheng mellom tid og radioaktivitet. Denne kan, i sin tur, brukast
+til å estimere halveringstida for den radioaktive isotopen.
+Vi gjer dette ved å bruke lineær regresjon med minste kvadraters metode
+på logaritmen (den naturlege logaritmen) på dei sampla data. 
 """
 
 # Bibliotek
@@ -44,19 +43,22 @@ print("b =", b)
 
 # Plottar punkta og regresjonslinja - for den lineære samanhengen
 plt.figure(1)
+plt.clf()
 plt.plot(tid,AktivLog,'rx', label = 'Logaritmen av A-data')  
 plt.plot(tid,a+b*tid,'k-', label = 'Lineær tilnærming')
 plt.legend()
 plt.xlabel('Tid')
 plt.ylabel('log(A)')
-plt.grid()                           		# Legg på rutenett  
-
+plt.grid(visible = True)                           		# Legg på rutenett  
+plt.show()
+ 
 # Plottar tilsvarande figur for den eksponentielle samanhengen
 plt.figure(2)
+plt.clf()
 plt.plot(tid,Aktiv,'rx', label = 'Datapunkt, A')  
 plt.plot(tid,np.exp(a+b*tid),'k-', label = 'Eksponentiell tilnærming')
 plt.legend()
 plt.xlabel('Tid')
 plt.ylabel('A(t)')
-plt.grid()                           		# Legg på rutenett  
-
+plt.grid(visible = True)                           		# Legg på rutenett  
+plt.show()
