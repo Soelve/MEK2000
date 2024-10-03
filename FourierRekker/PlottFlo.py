e0a4edf2873c156c5f2ca2305f95f8eded9a1519
Sølve Selstø <124062842+Soelve@users.noreply.github.com>
2024-10-03 19:16:38 +0200

    Update PlottFlo.py

--- FourierRekker/PlottFlo.py
+++ FourierRekker/PlottFlo.py
@@ -1,5 +1,4 @@
-"""
-Dette skriptet plottar ein modell for flo og fjoere
+"""Dette skriptet plottar ein modell for flo og fjoere.
 """
 
 # Bibliotek
@@ -10,13 +9,14 @@ t = np.linspace(0, 24, 200)
 V = 0.67*np.sin(0.51*t+0.7)
 
 # Plottar
-plt.close()
+plt.figure(1)
+plt.clf()
+# Plottar modellen
 plt.plot(t, V, 'b-')
-
 # Plottar tidspunkta då vannstanden er 0.5 m
 plt.hlines(0.5, 0, 24, colors = 'black', linestyles = 'dashed')
-"""
 plt.plot([0.28, 3.09, 12.60, 15.41], 0.5*np.ones(4), 'ro')
 plt.xlabel('Tid [timar]')
 plt.ylabel('Vannstand [meter]')
-"""
+plt.grid(visible=True)
+plt.show()
