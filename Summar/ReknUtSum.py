d741040953e55ddae549c2d318b65f257d75a6ca
Sølve Selstø <124062842+Soelve@users.noreply.github.com>
2024-10-21 22:43:42 +0200

    Update ReknUtSum.py

--- Summar/ReknUtSum.py
+++ Summar/ReknUtSum.py
@@ -9,20 +9,20 @@ starten - hardkoda det også.
 import numpy as np
 
 # Øvre grense
-N = 1000
+N = 100
 # Nedre grense
-n0 = 1
+n0 = 2
 
 # Initierar summen
 S = 0
 # Merk: For at for-løkka skal ta med N, må vi gå forbi i range-kallet
 for n in range(n0,N+1):
-  an = 1/n**2
+  an = 2/(n**2-1)
   S = S + an
 
 # Skriv svaret til skjerm:
 print(f'Sum: {S:.5f}')
 
 # Samanliknar med eventuell fasit
-Fasit = np.pi**2/6
+Fasit = 1.5
 print(f'Feil: {np.abs(Fasit-S):.5f}')
