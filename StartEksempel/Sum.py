733109e44305b2e8d0a8c78a51ed3550c446e6b3
Sølve Selstø <124062842+Soelve@users.noreply.github.com>
2025-08-13 14:32:52 +0200

    Update Sum.py

--- StartEksempel/Sum.py
+++ StartEksempel/Sum.py
@@ -1,8 +1,17 @@
-s = 0
+"""
+Dette skriptet reknar ut ein sum. Det tar eit algebraisk uttrykk for kvart
+ledd som input (linje 15). I tillegg tar det start- og sluttverdi for indeksen 
+n som input.
+"""
+
+# Initerarsummen
+S = 0
+
+# Gir start- og sluttverdi for indeksen
 n_start = 0
 n_max = 100
 
-for n in range(n_start, n_max+1):
-    s = s + n 
+for n in range(n_start, n_max+1):     # Dette med "+1" er ganske forvirrande
+    S = S + n**2 
     
-print('s = ', s)    
+print('S = ', S)    
