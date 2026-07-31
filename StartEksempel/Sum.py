3103dec5b1b64e66b10db0790975524801f4a8eb
Sølve Selstø <124062842+Soelve@users.noreply.github.com>
2026-07-31 13:49:01 +0200

    Update Sum.py

--- StartEksempel/Sum.py
+++ StartEksempel/Sum.py
@@ -4,14 +4,25 @@ ledd som input (linje 15). I tillegg tar det start- og sluttverdi for indeksen
 n som input.
 """
 
+# Importerar numpy - i fall vi treng det
+import numpy as np
+
 # Initerarsummen
 S = 0
 
 # Gir start- og sluttverdi for indeksen
-n_start = 0
-n_max = 100
+n_start = 1
+n_max = 10
 
+# Gir uttrykk for ledda
+def a(n):
+    return 1/n**3
+
+##############################################
+
+# Summerar alle ledda
 for n in range(n_start, n_max+1):     # Dette med "+1" er ganske forvirrande
-    S = S + n**2 
+    S = S + a(n)
     
+# Skriv svar til skjerm    
 print('S = ', S)    
