b003915526f6b028335b8b5383c3f6f4ff111507
Sølve Selstø <124062842+Soelve@users.noreply.github.com>
2026-07-31 13:49:49 +0200

    Update Sum.py

--- StartEksempel/Sum.py
+++ StartEksempel/Sum.py
@@ -7,18 +7,18 @@ n som input.
 # Importerar numpy - i fall vi treng det
 import numpy as np
 
-# Initerarsummen
-S = 0
-
 # Gir start- og sluttverdi for indeksen
 n_start = 1
-n_max = 10
+n_max = 5
 
 # Gir uttrykk for ledda
 def a(n):
-    return 1/n**3
+    return 1/(n**2+1)
 
 ##############################################
+
+# Initerarsummen
+S = 0
 
 # Summerar alle ledda
 for n in range(n_start, n_max+1):     # Dette med "+1" er ganske forvirrande
