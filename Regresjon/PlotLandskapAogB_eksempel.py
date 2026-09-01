e33d2a511d8a7e94584cc87878fe5295834121fd
Sølve Selstø <124062842+Soelve@users.noreply.github.com>
2026-09-01 13:14:36 +0200

    Update PlotLandskapAogB_eksempel.py

--- Regresjon/PlotLandskapAogB_eksempel.py
+++ Regresjon/PlotLandskapAogB_eksempel.py
@@ -3,34 +3,40 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 
 # Funksjon som vi vil plotte
-def funk(x,y):
+def funk(a,b):
 #    return x**2 + y**2
-    return (3-(x+ y*1))**2 + (4-(x+y*2.5))**2 + (4.5-(x+y*3))**2
+    return (3-(a+b*1))**2 + (4-(a+b*2.5))**2 + (4.5-(a+b*3))**2
 
 # Grenser for x og y (hardkoda)
-xMin = 0;
-xMax = 3;
-yMin = 0;
-yMax = 2;
+aMin = -2;
+aMax = 5;
+bMin = -2;
+bMax = 4;
 
 # Vektor - og matriser - med funksjonsverdiar (200 pkt i kvar retning)
-x = np.linspace(xMin, xMax, 200)
-y = np.linspace(yMin, yMax, 200)
-xx, yy = np.meshgrid(x, y)
+a = np.linspace(aMin, aMax, 200)
+b = np.linspace(bMin, bMax, 200)
+aa, bb = np.meshgrid(a, b)
 
 # Matrise med z-verdiar
-zz = funk(xx, yy)
+S_mat = funk(aa, bb)
 
 # Ny 3d-figur til å lage flateplott
 fig = plt.figure(1)
+plt.clf()
 ax = fig.add_subplot(111, projection='3d')
 # Lagar flateplott i rommet
-surf = ax.plot_surface(xx, yy, np.log(zz), cmap = cm.magma, alpha = .5)
+surf = ax.plot_surface(aa, bb, S_mat, cmap = cm.magma, alpha = .5)
 Aminval = 2.25
 Bminval = 0.731
-Cminval = funk(Aminval, Bminval)
-ax.plot(Aminval, Bminval, np.log(Cminval), 'rx')
+Sminval = funk(Aminval, Bminval)
+ax.plot(Aminval, Bminval, Sminval, 'rx')
 # Tekst på aksane
 ax.set_xlabel('a')
 ax.set_ylabel('b')
+plt.show()
+
+plt.figure(2)
+plt.clf()
+plt.pcolor(aa, bb, S_mat)
 plt.show()
