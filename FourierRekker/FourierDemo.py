4834088a269b62e8051c2c8cb37096ea2cdf133d
Sølve Selstø <124062842+Soelve@users.noreply.github.com>
2026-09-16 16:13:35 +0200

    Update FourierDemo.py

--- FourierRekker/FourierDemo.py
+++ FourierRekker/FourierDemo.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """Skriptet plottar ein periodisk funksjon saman med trunkterte Fourier-rekker 
 av stadig høgare orden.
 
@@ -21,30 +20,34 @@ T = 6
 w = 2*np.pi/T
 
 # Initierar vektor med 200 x-verdiar mellom -9 og 9
-Npkt = 200
+Npkt = 500
 x = np.linspace(-9, 9, Npkt)
 
 
-# Tilordnar a0 og initerar vektor for Fourier-summen. (Konstant lik a0.)
+# Tilordnar a0 og initerar vektor for Fourier-summen (konstant lik a0).
 a = 3/4
 y = a*np.ones(Npkt)
 n = 0
+plt.figure(1)
+
 # Itererar ved aa auke trunkeringsgrensa N
 while n <= Nmax: 
   # Nytt plott
-  plt.figure(1)
   plt.clf()
   # Plottar sjølve funksjonen (tre periodar)
   plt.plot([-3, 0, 3], [3, 0, 0], 'k-')
   plt.plot([3, 6, 9], [3, 0, 0], 'k-')
   plt.plot([-9, -6, -3], [3, 0, 0], 'k-')
+  
   # Plottar Fourier-summen for den aktuelle trunkeringa
   plt.plot(x, y, 'r--')
-  # Set tittelen
+  
+  # Pyntar på plottet
   TitleStr = f'Fourier-sum med N = {n}'
   plt.title(TitleStr)
   plt.grid(visible = True)          # Rutenett
-  plt.show()
+  plt.ylim(-0.5,3.5)
+  
   # Oppsdaterar n
   n = n+1
   # Oppdaterar koeffisientane a og b - og sjølve Fourier-summen y
@@ -53,5 +56,8 @@ while n <= Nmax:
   if np.mod(n,2) == 1:              # a-koeffisentane er null for partals-n
       a = -6/(n*np.pi)**2
       y = y + a*np.cos(n*w*x)
-  # Legg inn ein pause
-  dummy = input('Hit enter')
+  
+    # Legg inn ein pause
+  plt.show(block=False)
+  plt.pause(0.1)
+  plt.waitforbuttonpress()
