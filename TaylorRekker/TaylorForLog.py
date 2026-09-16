de41a525b462c71f10554f6f2ef86dd0a5d03d98
Sølve Selstø <124062842+Soelve@users.noreply.github.com>
2026-09-16 13:51:20 +0200

    Add files via upload

---
+++ TaylorRekker/TaylorForLog.py
@@ -0,0 +1,47 @@
+"""Skript som plottar Taylor-polynoma, opp til 
+ ein bestemt orden, for funksjonen
+ f(x) = ln x omkring x=1.
+ Den maksimale ordenen til funksjonen er input."""
+
+# Bibliotek
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Maksimal orden
+Nmax = 10
+
+# Funksjonen
+def funk(x):
+    return np.log(x)
+
+# Vektor med x-verdiar
+x = np.linspace(0, 3, 200)
+
+# Initierer polynomet
+P = 0 * x
+
+# Startar plot
+fig = plt.figure(1)
+
+# Går gjennom alle N-verdiane til og med Nmax
+for n in range(1,Nmax + 1):
+
+    # Neste ledd i Taylor-polynomet
+    an = (-1)**(n-1)/n
+    P = P + an * (x-1)**n
+
+    plt.clf()   # Tømmer figuren
+
+    plt.plot(x, funk(x), 'k-', linewidth=2)     # Plottar sjølve funksjonen
+
+    # Plottar Taylor-polynomet
+    plt.plot(x, P, 'r--', linewidth=2)          # Plottar tilnærminga
+    
+    plt.ylim(-5, 3)                              # Grenser for y-aksen
+    plt.grid(True)                              # Set på rutenett
+    plt.title(f'n = {n}')                       # Tittel på figuren
+
+    # Ventar på tastetrykk
+    plt.show(block=False)
+    plt.pause(0.1)
+    plt.waitforbuttonpress()
